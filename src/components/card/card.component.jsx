06a1c2ded55edd072b432a8ff112b4619e51907f
Shreyas75 <manotishreyas@gmail.com>
2023-01-07 18:17:25 +0530

    Monsters React App Final

---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,20 @@
+import { Component } from 'react';
+import './card.styles.css';
+
+class Card extends Component {
+
+    render() {
+        const { name, email, id } = this.props.monster;
+        return (
+            <div>
+                <div className='card-container' key={id}>
+                    <img alt={`monster ${'monster'}`} src={`https://robohash.org/${id}?set=set2&size=120x120`} />
+                    <h2>{name}</h2>
+                    <p>{email}</p>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Card;
